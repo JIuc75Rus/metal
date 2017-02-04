e3f21915c4ae80e88381a1eaabf36ea86e82d559
Evgeny <evgeny@evgeny@admin.ru>
2017-02-04 19:02:18 +0900

    ready

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
   def page_not_found
     respond_to do |format|
       format.html { render template: 'pages/404', layout: 'layouts/application', status: 404 }
       format.all  { render nothing: true, status: 404 }
     end
   end
+
   def internal_server_error
     respond_to do |format|
       format.html {render template: 'pages/404', layout: 'layouts/application', status: 500 }
