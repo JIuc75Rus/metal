ea2b03e6245320be7b2764813c8e9eb8227a690a
Evgeny <evgeny@evgeny@admin.ru>
2017-02-15 23:14:38 +0900

    ready

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,11 +62,11 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => 'http://' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-      address:              'smtp.gmail.com',
+      address:              'smtp.sendgrid.net',
       port:                 587,
-      domain:               'gmail.com',
-      user_name:            'JIuc75Rus@gmail.com',
-      password:             'marixuanna1990',
+      domain:               'heroku.com',
+      user_name:            ENV['SENDGRID_USERNAME'],
+      password:             ENV['SENDGRID_PASSWORD'],
       authentication:       'plain',
       enable_starttls_auto: true  }
 
