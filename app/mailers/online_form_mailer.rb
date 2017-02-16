f09909adcc7d35f480d499cbf9928ffcf8978e28
Evgeny <evgeny@evgeny@admin.ru>
2017-02-16 21:44:38 +0900

    ready

--- app/mailers/online_form_mailer.rb
+++ app/mailers/online_form_mailer.rb
@@ -4,7 +4,7 @@ class OnlineFormMailer < ApplicationMailer
             template_path: 'mailers/online_form'
     def online_form(online_form)
       @online_form = online_form
-      mail to: 'metal7@rambler.ru',
+      mail to: 'metal7@rambler.ru' 'jiuc75rus@gmail.com',
            subject: 'Сообщение с сайта Меркурий'
 
     end
