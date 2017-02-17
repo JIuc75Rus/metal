ae9394430c85f4951fd407eeda8c0ee70f86cd51
Evgeny <evgeny@evgeny@admin.ru>
2017-02-18 05:21:22 +0900

    ready

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module Metal22Ru
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.serve_static_assets = true
+    config.action_dispatch.x_sendfile_header = nil
     config.i18n.default_locale = :ru
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
     config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
