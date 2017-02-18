a9d2ee465a33a18c8f22125e93aad1ea2fd5f479
Evgeny <evgeny@evgeny@admin.ru>
2017-02-18 09:25:28 +0900

    ready

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,6 @@
 module ApplicationHelper
+  require 'rubygems'
+  require 'geoip'
   def urls_to_images(s)
     s.gsub! /\s|(http:\/\/.*?\.(png|jpg|jpeg))(\s|\Z)/,
             '<img src="\1"/ id="something_unique" class="thumb">'
@@ -10,5 +12,14 @@ module ApplicationHelper
             '<a href="\1"/>\1</a>'
     s.html_safe
   end
-
+  def geo(i)
+     @geoip ||= GeoIP.new("#{Rails.root}/db/GeoLiteCity.dat")
+     remote_ip = request.remote_ip
+     if remote_ip != "127.0.0.1" #todo: check for other local addresses or set default value
+       location_location = @geoip.country(remote_ip)
+       if location_location != nil
+         @model.country = location_location[2]
+       end
+     end
+  end
 end
