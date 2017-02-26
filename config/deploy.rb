25243ad0e2dd381af75b36ee273f00b834dec2af
Evgeny <evgeny@evgeny@admin.ru>
2017-02-26 18:36:33 +0900

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,8 @@
-set :application, 'metal22'
+# config valid only for current version of Capistrano
+lock "3.7.2"
+
+set :application, 'application'
 set :repo_url, "https://github.com/JIuc75Rus/metal.git"
-set :deploy_to, '/var/www/metal22'
 set :unicorn_config_path, "#{current_path}/config/production/unicorn/unicorn.rb"
 set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system') # Строчка есть по умолчанию в deploy.rb, ее просто надо откомментировать
 namespace :deploy do
@@ -23,3 +25,31 @@ namespace :deploy do
 end
 before :deploy, 'git:push'
 before 'deploy:setup', 'git:push'
+
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+# Default deploy_to directory is /var/www/my_app_name
+# set :deploy_to, "/var/www/my_app_name"
+
+# Default value for :format is :airbrussh.
+# set :format, :airbrussh
+
+# You can configure the Airbrussh format using :format_options.
+# These are the defaults.
+# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+# append :linked_files, "config/database.yml", "config/secrets.yml"
+
+# Default value for linked_dirs is []
+# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
