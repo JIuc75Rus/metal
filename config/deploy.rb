c6a93d544ebf7ce08568f5a62f34966be1971dc9
Evgeny <evgeny@evgeny@admin.ru>
2017-02-28 16:02:34 +0900

    deploy

---
+++ config/deploy.rb
@@ -0,0 +1,34 @@
+
+# Deploy with Ansible on a new server:
+#   $ (cd config/provision && ansible-playbook -i178.62.254.62, playbook.yml)
+#   $ bundle exec cap production deploy
+#   $ bundle exec cap production rake task=db:seed
+
+lock '3.6.0'
+
+set :application, 'metal22'
+set :repo_url, 'https://github.com/JIuc75Rus/metal.git'
+set :branch, 'master'
+set :deploy_to, '/home/deploy/applications/metal22'
+
+set :log_level, :info
+set :linked_files, %w{config/database.yml}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/uploads}
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.3.3'
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_roles, :all
+
+set :puma_init_active_record, true
+
+desc 'Run rake tasks on server'
+task :rake do
+  on roles(:app), in: :sequence, wait: 5 do
+    within release_path do
+      with rails_env: :production do
+        execute :rake, ENV['task'], 'RAILS_ENV=production'
+      end
+    end
+  end
+end
---
+++ config/deploy/production.rb
@@ -0,0 +1,10 @@
+ip = '195.133.146.223'
+
+role :app, ["deploy@#{ip}"]
+role :web, ["deploy@#{ip}"]
+role :db,  ["deploy@#{ip}"]
+
+server ip, user: 'deploy', roles: %w{web app db}
+
+set :stage, 'production'
+set :rails_env, 'production'
