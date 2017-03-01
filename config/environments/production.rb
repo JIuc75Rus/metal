50d460d1f4a2157b2e8650cac7144b6c5d30f592
Evgeny <evgeny@evgeny@admin.ru>
2017-03-01 13:07:48 +0900

    deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,12 +19,6 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
-  if ENV["RAILS_LOG_TO_STDOUT"].present?
-    logger           = ActiveSupport::Logger.new(STDOUT)
-    logger.formatter = config.log_formatter
-    config.logger = ActiveSupport::TaggedLogging.new(logger)
-  end
-
   config.serve_static_assets = true
   config.action_dispatch.x_sendfile_header = nil
 
