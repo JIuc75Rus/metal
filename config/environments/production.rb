25329f471b19371c03c1eb1aa7c7807472880220
Evgeny <evgeny@evgeny@admin.ru>
2017-03-01 18:08:27 +0900

    deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ Rails.application.configure do
   config.public_file_server.enabled = true
   config.assets.compile = true
   config.serve_static_assets = true
-  config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+  config.assets.precompile = true
 
 
   # Compress JavaScripts and CSS.
