35ee18a4746ec492e30b9b39826b9cf286ecb4bb
Evgeny <evgeny@evgeny@admin.ru>
2017-03-01 15:02:27 +0900

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,10 +15,6 @@ set :log_level, :info
 set :linked_files, %w{config/database.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/uploads}
 
-set :log_level, :info
-set :linked_files, %w{config/database.yml}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/uploads}
-
 set :rbenv_type, :user
 set :rbenv_ruby, '2.3.3'
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
