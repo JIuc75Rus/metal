f73b1ecb2144ca2bafe4b7986993e950abcf29f8
Evgeny <evgeny@evgeny@admin.ru>
2017-03-01 13:10:52 +0900

    deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,7 +18,7 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
-
+  config.assets.compile = true
   config.serve_static_assets = true
   config.action_dispatch.x_sendfile_header = nil
 
