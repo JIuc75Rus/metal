d370f4e4f7f1bd1707adce58610a54de8c196990
Evgeny <evgeny@evgeny@admin.ru>
2017-03-09 14:55:34 +0900

    deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,9 +33,6 @@ Rails.application.configure do
   }
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
   config.action_mailer.raise_delivery_errors = false
-
-  config.action_mailer.perform_caching = false
-  config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :sendmail
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
