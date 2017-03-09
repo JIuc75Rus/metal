6b3888b0dbb68900f904c250e30bda43116fffc6
Evgeny <evgeny@evgeny@admin.ru>
2017-03-09 14:32:57 +0900

    deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,16 +36,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { :host => 'http://' }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-      address:              'smtp.sendgrid.net',
-      port:                 587,
-      domain:               'heroku.com',
-      user_name:            ENV['SENDGRID_USERNAME'],
-      password:             ENV['SENDGRID_PASSWORD'],
-      authentication:       'plain',
-      enable_starttls_auto: true  }
+  config.action_mailer.delivery_method = :sendmail
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
