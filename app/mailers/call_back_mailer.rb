9c032fce949cf337db0974500eb1b912ba08b9ee
Evgeny <evgeny@evgeny@admin.ru>
2017-03-09 18:09:45 +0900

    deploy

--- app/mailers/call_back_mailer.rb
+++ app/mailers/call_back_mailer.rb
@@ -1,6 +1,6 @@
 class CallBackMailer < ApplicationMailer
 
-    default from: '"Сообщение" - metal22.ru',
+    default from: 'metal22.ru',
             template_path: 'mailers/call_back'
 
     def call_back(call_back)
