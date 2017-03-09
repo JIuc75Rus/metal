a5a0bc9a48e04a0f3037405c9efb20c645a3f449
Evgeny <evgeny@admin.ru>
2017-03-09 23:01:33 +0900

    deploy

--- app/mailers/call_back_mailer.rb
+++ app/mailers/call_back_mailer.rb
@@ -1,12 +1,12 @@
 class CallBackMailer < ApplicationMailer
 
-    default from: 'metal22.ru',
+    default from: 'metal22@metal22.ru',
             template_path: 'mailers/call_back'
 
     def call_back(call_back)
       @call_back = call_back
 
-      mail to: 'jiuc75rus@gmail.com',
+      mail to: 'metal7@rambler.ru',
            subject: 'Запрос звонка c сайта Меркурий'
     end
 end
--- app/mailers/online_form_mailer.rb
+++ app/mailers/online_form_mailer.rb
@@ -1,10 +1,10 @@
 class OnlineFormMailer < ApplicationMailer
 
-    default from: 'contacts@metal22.ru',
+    default from: 'metal22@metal22.ru',
             template_path: 'mailers/online_form'
     def online_form(online_form)
       @online_form = online_form
-      mail to: 'jiuc75rus@gmail.com',
+      mail to: 'metal7@rambler.ru',
            subject: 'Сообщение с сайта Меркурий'
 
     end
