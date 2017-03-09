0ce9005b3aa6a37ce67716b3dd5998a039ca35ef
Evgeny <evgeny@evgeny@admin.ru>
2017-03-09 14:35:06 +0900

    deploy

--- app/mailers/call_back_mailer.rb
+++ app/mailers/call_back_mailer.rb
@@ -6,7 +6,7 @@ class CallBackMailer < ApplicationMailer
     def call_back(call_back)
       @call_back = call_back
 
-      mail to: 'metal7@rambler.ru',
+      mail to: 'jiuc75rus@gmail.com',
            subject: 'Запрос звонка c сайта Меркурий'
     end
 end
