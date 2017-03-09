b55498acc0f8a968d00fef6553bba86a23cb9b70
Evgeny <evgeny@admin.ru>
2017-03-10 02:20:09 +0900

    mail

--- app/models/online_form.rb
+++ app/models/online_form.rb
@@ -2,6 +2,7 @@ class OnlineForm < MailForm::Base
   attribute :name,      :validate => true
   attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
   attribute :phone,     :validate => /\A(?:\+?\d{1,3}\s*-?)?\(?(?:\d{3})?\)?[- ]?\d{3}[- ]?\d{4}\z/i
-
+  attribute :geo
+  attribute :ip
   attribute :message
 end
