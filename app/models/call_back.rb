1eead644f534af3c2b6332c37bfc361743cbbb12
JIuc75Rus <jiuc75pyc@gmail.com>
2017-05-03 16:11:45 +0900

    Update call_back.rb

--- app/models/call_back.rb
+++ app/models/call_back.rb
@@ -1,7 +1,9 @@
 class CallBack < MailForm::Base
   attribute :name,      :validate => true
   attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
-  attribute :phone,     :validate => /\A(?:\+?\d{1,3}\s*-?)?\(?(?:\d{3})?\)?[- ]?\d{3}[- ]?\d{4}\z/i
+  attribute :phone,     :validate => /\A(?:\+?\d{1,3}\s*-?)?\(?(?:\d{3})?\)?[- ]?\d{3}[- ]?\d{4}\z/i,
+                        :numericality => true,
+                        :length => { :minimum => 10, :maximum => 15 }
   attribute :geo
   attribute :ip
   attribute :message
